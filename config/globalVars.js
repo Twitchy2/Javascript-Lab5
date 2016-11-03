8f4ac5eb3cc796e43b97c22346b806ffac6fd785
Twitchy2 <sourceman2@gmail.com>
2016-11-02 21:59:55 -0400

    Final

--- config/globalVars.js
+++ config/globalVars.js
@@ -2,7 +2,11 @@
 //start with the db connection string
 
 module.exports = {
-  db: 'mongodb://localhost/test',
+  secret: 'The power of (((them)))',
+db: 'mongodb://Twitch:Twitch@ds050189.mlab.com:50189/users'
+
+  //db: 'mongodb://localhost/test',
   //db: 'MLabs databse goes here'
-  secret: 'The power of (((them)))'
+
+  //db: 'mongo ds050189.mlab.com:50189/users -u <logan> -p <logan>'
 }
--- routes/users.js
+++ routes/users.js
@@ -24,7 +24,6 @@ router.get('/', function(req, res, next) {
        });
     }
   })
-
 });
 
 module.exports = router;
